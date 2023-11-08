d49107516be304dd5799d8216f194e76ceae856e
kkk2z <139834827+kkk2z@users.noreply.github.com>
2023-11-08 09:12:31 +0900

    Create 1.py

---
+++ revolt/1.py
@@ -0,0 +1,56 @@
+import revolt
+import asyncio
+
+# Revoltボットのトークンを入力してください
+TOKEN = "your_bot_token"
+
+# Revoltクライアントを作成します
+client = revolt.Client()
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!ban"):
+        # ユーザーをBANします
+        if "admin" in [role.name for role in message.author.roles]:
+            if message.mentions:
+                for user in message.mentions:
+                    await client.ban_user(user, message.channel)
+            else:
+                await message.channel.send("メンションでユーザーを指定してください")
+        else:
+            await message.channel.send("管理者専用コマンドです")
+
+    elif message.content.startswith("!kick"):
+        # ユーザーをキックします
+        if "admin" in [role.name for role in message.author.roles]:
+            if message.mentions:
+                for user in message.mentions:
+                    await client.kick_user(user, message.channel)
+            else:
+                await message.channel.send("メンションでユーザーを指定してください")
+        else:
+            await message.channel.send("管理者専用コマンドです")
+  
+    elif message.content.startswith("!timeout"):
+        # ユーザーをタイムアウトします
+        if "admin" in [role.name for role in message.author.roles]:
+            if message.mentions:
+                for user in message.mentions:
+                    await client.timeout_user(user, message.channel)
+            else:
+                await message.channel.send("メンションでユーザーを指定してください")
+        else:
+            await message.channel.send("管理者専用コマンドです")
+
+    elif message.content.startswith("!hello"):
+        # BOTからの応答
+        await message.channel.send("こんにちは！")
+  
+    # その他のコマンドやメッセージに対する処理を追加できます
+
+# Revoltに接続します
+client.run(TOKEN)
